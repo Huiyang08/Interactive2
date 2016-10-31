f2f5b415347b7b5e410e35711839756e9e0405d2
Huiyang08 <huiyang@HuimatoMacBook-Pro.local>
2016-10-31 15:00:41 -0700

    change ni

---
+++ P2/assets/js/script.js
@@ -0,0 +1,178 @@
+$(document).ready(function(){
+
+	// try uncommenting (deleting the // before a line) to see its effects
+
+	// $('.fill').addClass('shrink');	
+
+ //  	function() changeBackground(color) {
+ //   		document.body.style.background = color;
+	// }
+
+
+
+	//gray
+
+	$('#parallelogram').click(function() {
+		$('body').removeClass();
+		$('body').addClass('gray');
+   });
+
+	$('#rectangleZ1').click(function() {
+		$('body').removeClass();
+		$('body').addClass('gray');
+   });
+
+	$('#egg').click(function() {
+		$('body').removeClass();
+		$('body').addClass('gray');
+   });
+
+	$('#oval').click(function() {
+		$('body').removeClass();
+		$('body').addClass('gray');
+   });
+
+	$('#trapezoid3').click(function() {
+		$('body').removeClass();
+		$('body').addClass('gray');
+   });
+
+	$('#rectangleE').click(function() {
+		$('body').removeClass();
+		$('body').addClass('gray');
+   });
+	
+
+
+	//lightgray
+	$('#rectangleF').click(function() {
+		$('body').removeClass();
+		$('body').addClass('lightgray');
+   });
+
+	$('#rectangleL').click(function() {
+		$('body').removeClass();
+		$('body').addClass('lightgray');
+   });
+
+	$('#eggR').click(function() {
+		$('body').removeClass();
+		$('body').addClass('lightgray');
+   });
+
+
+	$('#rectangleY').click(function() {
+		$('body').removeClass();
+		$('body').addClass('lightgray');
+   });
+
+
+	$('#rectangleH').click(function() {
+		$('body').removeClass();
+		$('body').addClass('lightgray');
+   });
+
+
+	$('#parallelogram1').click(function() {
+		$('body').removeClass();
+		$('body').addClass('lightgray');
+   });
+
+
+//darkgray
+	$('#rectangle').click(function() {
+		$('body').removeClass();
+		$('body').addClass('darkgray');
+   });
+
+	$('#eggS1').click(function() {
+		$('body').removeClass();
+		$('body').addClass('darkgray');
+   });
+
+	$('#eggU').click(function() {
+		$('body').removeClass();
+		$('body').addClass('darkgray');
+   });
+
+	$('#eggP').click(function() {
+		$('body').removeClass();
+		$('body').addClass('darkgray');
+   });
+
+	$('#rectangleK1').click(function() {
+		$('body').removeClass();
+		$('body').addClass('darkgray');
+   });
+
+$('#ovalO1').click(function() {
+		$('body').removeClass();
+		$('body').addClass('darkgray');
+   });
+
+$('#parallelogramW1').click(function() {
+		$('body').removeClass();
+		$('body').addClass('darkgray');
+   });
+
+//white
+	$('#trapezoid').click(function() {
+		$('body').removeClass();
+		$('body').addClass('white');
+   });
+
+	$('#ovalG').click(function() {
+		$('body').removeClass();
+		$('body').addClass('white');
+   });
+
+
+	$('#rectangleZ').click(function() {
+		$('body').removeClass();
+		$('body').addClass('white');
+   })
+
+
+	$('#ovalO').click(function() {
+		$('body').removeClass();
+		$('body').addClass('white');
+   })
+
+	$('#parallelogramV').click(function() {
+		$('body').removeClass();
+		$('body').addClass('white');
+   })
+
+
+	
+
+ 	
+    
+
+	
+		
+ 
+   
+
+	// $('.fill').hover(function() {
+	// 	$(this).toggleClass('rotate');
+	// });	
+
+	// $('#letter').click(function() {
+	// 	var letter = $('#letter').clone();
+	// 	$('body').append(letter);
+	// });	
+
+
+	// random backgrounds over time
+	// setInterval(function(){
+	// 	$redValue = Math.floor(Math.random()*255);
+	// 	$greenValue = Math.floor(Math.random()*255);
+	// 	$blueValue = Math.floor(Math.random()*255);	
+	// 	$('.fill').css(
+	// 		'background', 'rgba('+$redValue+','+$blueValue+','+$greenValue+', 1)'
+	// 	);
+	// },500);
+	
+
+});
